4b22a3aff437d8a4916dfe3e84a72aec879b6146
Thedsanamoorthysarma Thanushika <121013173+Thanu1611@users.noreply.github.com>
2023-01-02 15:01:22 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -5,8 +5,8 @@ Class sum
         int a=3;
         int b=7;
         int d=10;
-        int e=16;
         int c=13;
         System.out.print(a+b);//summation
+        System.out.print(a+b+c+d);
     }
 }
